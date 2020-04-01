6002bf4d85b36ac5be4cbb35f02ee1148a324cb9
ryanrych <ryanjrychlak@gmail.com>
2020-04-01 02:25:14 -0500

    Added the ability to block stations and edges to the shortest path algorithm.

--- Edge.h
+++ Edge.h
@@ -6,13 +6,21 @@
 #define SHORTESTPATHTICKETTORIDE_EDGE_H
 
 struct Edge{
+    string start;
     string destination;
     int weight;
     Edge(){
+        start="";
         destination="";
         weight=-1;
     }
     Edge(string destination, int weight){
+        start="";
+        this->destination=destination;
+        this->weight=weight;
+    }
+    Edge(string start,string destination,int weight){
+        this->start=start;
         this->destination=destination;
         this->weight=weight;
     }
@@ -20,7 +28,7 @@ struct Edge{
         return (obj.weight<=this->weight);
     }
     bool operator==(const Edge& obj) const{
-        return (obj.weight==this->weight)&&(obj.destination.compare(obj.destination)==0);
+        return ((obj.weight==this->weight)&&(obj.destination.compare(this->destination)==0) && (obj.start.compare(this->start)==0));
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -7,14 +7,20 @@
 using namespace std;
 
 //add no repeating nodes and cumulative edge weights
-vector<string> dijkstras(map<string,OutDegree> graph,tuple<string,OutDegree> s,tuple<string,OutDegree> e){
+vector<string> dijkstras(map<string,OutDegree> graph,tuple<string,OutDegree> s,tuple<string,OutDegree> e,vector<string> closedStations,vector<Edge> closedTracks){
     PriorityQueue q;
     map<string,string> parent;
     map<string,int> shortest;
     vector<string> closed;
     closed.push_back(get<0>(s));
+    for (int i=0;i<closedStations.size();i++){
+        closed.push_back(closedStations.at(i));
+    }
 
     for (int i=0;i<get<1>(s).outDegree;i++){
+        if ((find(closed.begin(),closed.end(),get<1>(s).out[i].destination)!=closed.end()) || (find(closedTracks.begin(),closedTracks.end(),get<1>(s).out[i])!=closedTracks.end())){
+            continue;
+        }
         q.insert(get<1>(s).out[i].destination,get<1>(s).out[i].weight);
         parent.insert(pair<string,string>(get<1>(s).out[i].destination,get<0>(s)));
         shortest.insert(pair<string,int>(get<1>(s).out[i].destination,get<1>(s).out[i].weight));
@@ -24,7 +30,7 @@ vector<string> dijkstras(map<string,OutDegree> graph,tuple<string,OutDegree> s,t
         string best=q.getMinimumItem();
         Edge* bestOut=graph.at(best).out;
         for (int i=0;i<graph.at(best).outDegree;i++){
-            if (find(closed.begin(),closed.end(),bestOut[i].destination)!=closed.end()){
+            if ((find(closed.begin(),closed.end(),bestOut[i].destination)!=closed.end()) || (find(closedTracks.begin(),closedTracks.end(),bestOut[i])!=closedTracks.end())){
                 continue;
             }
             if (q.contains(bestOut[i].destination)){
@@ -70,7 +76,7 @@ int main() {
     board.insert(make_pair<string,OutDegree>("Seattle",OutDegree(new Edge[4]{Edge("Vancouver",1),Edge("Portland",1),Edge("Calgary",4),Edge("Helena",6)},4)));
     board.insert(make_pair<string,OutDegree>("Helena",OutDegree(new Edge[7]{Edge("Calgary",4),Edge("Seattle",6),Edge("Salt Lake City",3),Edge("Denver",4),Edge("Omaha",5),Edge("Duluth",6),Edge("Winnipeg",4)},7)));
     board.insert(make_pair<string,OutDegree>("Duluth",OutDegree(new Edge[6]{Edge("Winnipeg",4),Edge("Sault St.Marie",3),Edge("Toronto",6),Edge("Chicago",3),Edge("Omaha",2),Edge("Helena",6)},6)));
-    board.insert(make_pair<string,OutDegree>("Toronto",OutDegree(new Edge[4]{Edge("Sault St.Marie",2),Edge("Duluth",6),Edge("Montreal",3),Edge("Pittsburgh",2)},4)));
+    board.insert(make_pair<string,OutDegree>("Toronto",OutDegree(new Edge[5]{Edge("Sault St.Marie",2),Edge("Duluth",6),Edge("Montreal",3),Edge("Pittsburgh",2),Edge("Chicago",4)},5)));
     board.insert(make_pair<string,OutDegree>("New York",OutDegree(new Edge[4]{Edge("Montreal",3),Edge("Boston",2),Edge("Pittsburgh",2),Edge("Washington",2)},4)));
     board.insert(make_pair<string,OutDegree>("Portland",OutDegree(new Edge[3]{Edge("Seattle",1),Edge("Salt Lake City",6),Edge("San Francisco",5)},3)));
     board.insert(make_pair<string,OutDegree>("Salt Lake City",OutDegree(new Edge[5]{Edge("Portland",6),Edge("San Francisco",5),Edge("Helena",3),Edge("Denver",3),Edge("Las Vegas",3)},5)));
@@ -80,7 +86,7 @@ int main() {
     board.insert(make_pair<string,OutDegree>("Chicago",OutDegree(new Edge[5]{Edge("Duluth",3),Edge("Toronto",4),Edge("Pittsburgh",3),Edge("Saint Louis",2),Edge("Omaha",4)},5)));
     board.insert(make_pair<string,OutDegree>("Saint Louis",OutDegree(new Edge[5]{Edge("Kansas City",2),Edge("Chicago",2),Edge("Pittsburgh",5),Edge("Nashville",2),Edge("Little Rock",2)},5)));
     board.insert(make_pair<string,OutDegree>("Pittsburgh",OutDegree(new Edge[7]{Edge("Saint Louis",5),Edge("Chicago",3),Edge("Washington",2),Edge("New York",2),Edge("Nashville",4),Edge("Raleigh",2),Edge("Toronto",2)},7)));
-    board.insert(make_pair<string,OutDegree>("Nashville",OutDegree(new Edge[5]{Edge("Atlanta",1),Edge("Saint Louis",2),Edge("Raleigh",2),Edge("Little Rock",3),Edge("Pittsburgh",4)},5)));
+    board.insert(make_pair<string,OutDegree>("Nashville",OutDegree(new Edge[5]{Edge("Atlanta",1),Edge("Saint Louis",2),Edge("Raleigh",3),Edge("Little Rock",3),Edge("Pittsburgh",4)},5)));
     board.insert(make_pair<string,OutDegree>("Washington",OutDegree(new Edge[3]{Edge("Pittsburgh",2),Edge("Raleigh",2),Edge("New York",2)},3)));
     board.insert(make_pair<string,OutDegree>("Raleigh",OutDegree(new Edge[5]{Edge("Nashville",3),Edge("Pittsburgh",2),Edge("Atlanta",2),Edge("Charleston",2),Edge("Washington",2)},5)));
     board.insert(make_pair<string,OutDegree>("Atlanta",OutDegree(new Edge[5]{Edge("Charleston",2),Edge("Raleigh",2),Edge("New Orleans",4),Edge("Nashville",1),Edge("Miami",5)},5)));
@@ -98,10 +104,12 @@ int main() {
     board.insert(make_pair<string,OutDegree>("Los Angeles",OutDegree(new Edge[4]{Edge("San Francisco",3),Edge("Las Vegas",2),Edge("Phoenix",3),Edge("El Paso",6)},4)));
     board.insert(make_pair<string,OutDegree>("San Francisco",OutDegree(new Edge[3]{Edge("Portland",5),Edge("Los Angeles",3),Edge("Salt Lake City",5)},3)));
 
-    string start="El Paso";
-    string end="Kansas City";
+    string start="Vancouver";
+    string end="Miami";
+    vector<string> blockedStations={"Seattle","Helena","Omaha","Kansas City","Nashville","Atlanta","Duluth","Toronto","Raleigh","Charleston","Dallas","Santa Fe","Phoenix","Las Vegas"};
+    vector<Edge> blockedTracks={Edge("New Orleans",3),Edge("El Paso",5)};
 
-    vector<string> test=dijkstras(board,tuple<string,OutDegree>(start,board.at(start)),tuple<string,OutDegree>(end,board.at(end)));
+    vector<string> test=dijkstras(board,tuple<string,OutDegree>(start,board.at(start)),tuple<string,OutDegree>(end,board.at(end)),blockedStations,blockedTracks);
 
     for (int i=0;i<test.size();i++){
         cout<<test.at(i)<<endl;
